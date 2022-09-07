91e99bf1b8f609de83e24663211e2f9f275402c6
luckkalpna <107942615+luckkalpna@users.noreply.github.com>
2022-09-07 17:30:35 +0530

    practice set

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Game Constant & Variable
+let direction = {x: 0, y: 0};
+const foodSound = new Audio('food.mp3');
+const gameOverSound = new Audio('game-over.mp3');
+const musicSound = new Audio('background.mp3');
+
+
+
+
+
+
+
+// Game Functions
+
+
+
+
+
+
+
+
+
+
+// Main Logics started here
